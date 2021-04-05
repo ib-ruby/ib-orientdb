20c9766c2cde13333bdf3fa83a1acfa917b0d08e
Hartmut Bischoff <topofocus@gmail.com>
2021-04-05 18:25:22 +0200

    Update setup-orientdb.rb

--- lib/ib/setup-orientdb.rb
+++ lib/ib/setup-orientdb.rb
@@ -65,55 +65,55 @@ module Setup
      	ActiveOrient::Init.define_namespace { IB }
 
 			### Vertices
-		  V.create_class            :contract	, :account  
-			IB::Account.create_class  :advisor, :user
-			IB::Advisor.create_class  :demo_advisor
-			IB::User.create_class     :demo_user
-			V.create_class            :portfolio_value, :account_value,  :underlying, :contract_detail, :bar, :combo_leg
-			IB::Contract.create_class :option, :future, :stock, :forex, :index, :bag
-			IB::Bag.create_class      :spread
+		V.create_class            :contract	, :account  
+		IB::Account.create_class  :advisor, :user
+		IB::Advisor.create_class  :demo_advisor
+		IB::User.create_class     :demo_user
+		V.create_class            :portfolio_value, :account_value,  :underlying, 
+		                          :contract_detail, :bar, :combo_leg
+		IB::Contract.create_class :option, :future, :stock, :forex, :index, :bag
+		IB::Bag.create_class      :spread
 						
-		  IB::Contract.create_property :con_id, type: :integer, index: :unique
-		  IB::Contract.create_property :bars, type: :link_list,  index: :notunique, 
-																										 linked_class: IB::Bar
-		  IB::Contract.create_property :contract_detail, type: :link,  index: :notunique, 
-																										 linked_class: IB::ContractDetail
-
-			IB::Account.create_property  :account, type: :string, index: :unique
-			IB::Account.create_property  :last_access, type: :date
-			IB::User.create_property     :watchlist, type: :map
-			IB::PortfolioValue.create_property  :position, type: :decimal
-			IB::PortfolioValue.create_property  :market_price, type: :decimal
-			IB::PortfolioValue.create_property  :market_value, type: :decimal
-			IB::PortfolioValue.create_property  :average_cost, type: :decimal
-			IB::PortfolioValue.create_property  :realized_pnl, type: :decimal
-			IB::PortfolioValue.create_property  :unrealized_pnl, type: :decimal
-		  IB::PortfolioValue.create_property  :contract, type: :link, index: :notunique, linked_class: IB::Contract
+		IB::Contract.create_property :con_id, type: :integer, index: :unique
+		IB::Contract.create_property :bars, type: :link_list,  index: :notunique, 																					 linked_class: IB::Bar
+		IB::Contract.create_property :contract_detail, type: :link,  index: :notunique, 
+		  			      linked_class: IB::ContractDetail
+		IB::Account.create_property  :account, type: :string, index: :unique
+		IB::Account.create_property  :last_access, type: :date
+		IB::User.create_property     :watchlist, type: :map
+		IB::PortfolioValue.create_property  :position, type: :decimal
+		IB::PortfolioValue.create_property  :market_price, type: :decimal
+		IB::PortfolioValue.create_property  :market_value, type: :decimal
+		IB::PortfolioValue.create_property  :average_cost, type: :decimal
+		IB::PortfolioValue.create_property  :realized_pnl, type: :decimal
+		IB::PortfolioValue.create_property  :unrealized_pnl, type: :decimal
+		IB::PortfolioValue.create_property  :contract, type: :link, index: :notunique, 
+		                                     linked_class: IB::Contract
 
      	ActiveOrient::Init.define_namespace { HC }
-			V.create_class :portfolio 
-			HC::Portfolio.create_property :account, type: :link,  index: :notunique, linked_class: IB::Account
-			HC::Portfolio.create_property :positions, type: :link_list, linked_class: IB::PortfolioValue
-			HC::Portfolio.create_property :values, type:  :map 
+		V.create_class :portfolio 
+		HC::Portfolio.create_property :account, type: :link,  index: :notunique, linked_class: IB::Account
+		HC::Portfolio.create_property :positions, type: :link_list, linked_class: IB::PortfolioValue
+		HC::Portfolio.create_property :values, type:  :map 
 			#
 			## Edges   (Namespace HC)
-			E.create_class :has_portfolio, :has_position,  :grid, :has_account, :has_strategy
-			E.create_class  :d2F, :p2U, :my_user
-			HC::GRID.uniq_index
-			HC::D2F.uniq_index
-			HC::MY_USER.uniq_index
+		E.create_class :has_portfolio, :has_position,  :grid, :has_account, :has_strategy
+		E.create_class  :d2F, :p2U, :my_user
+		HC::GRID.uniq_index
+		HC::D2F.uniq_index
+		HC::MY_USER.uniq_index
 	end
 
 	def self.init_timegraph
 			## Initialize TimeGraph
-			TG::Setup.init_database 
-			TG::TimeGraph.populate 1980..2030
-			TG.info
+		TG::Setup.init_database 
+		TG::TimeGraph.populate 1980..2030
+		TG.info
 	end
 
 	def self.clear_database 
 		IB::Account.delete  all: true
-    IB::Contract.delete all: true
+                IB::Contract.delete all: true
 	end
 
 end
